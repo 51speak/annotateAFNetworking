77ddd2df97c54c5d8391a14937d33e8f790af578
yangyu <yangyu0525@autohome.com.cn>
2020-03-16 14:33:14 +0800

    feat:提交

---
+++ annotateAFNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  annotateAFNetworking
+//
+//  Created by yangyu on 2020/3/16.
+//  Copyright © 2020 yangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ annotateAFNetworking/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  annotateAFNetworking
+//
+//  Created by yangyu on 2020/3/16.
+//  Copyright © 2020 yangyu. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AFNetworking.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+
+@end
